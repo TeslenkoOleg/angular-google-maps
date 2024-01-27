53f40d993fe81f067f0f8f916db51314a73b72de
oleg_decenterads <oleg.teslenko@decenterads.com>
2024-01-27 11:26:39 +0200

    Add init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AngularGoogleMapsComponent} from "./angular-google-maps/angular-google-maps.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AngularGoogleMapsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
